1b33022eaca7b6b036319ac50d95c7087545252f
NikP295 <nikposedi295@gmail.com>
2025-04-25 01:25:58 +0200

    Commit timestamp: pet 25 apr 2025 01:25:58 CEST

--- push_to_git.sh
+++ push_to_git.sh
@@ -4,7 +4,7 @@ current_time=$(date)
 
 sudo git commit -m "Commit timestamp: $current_time"
 
-sudo git push -u origin main
+sudo -E git push -u origin main
 
 echo "Big W, associated timestamp: $current_time"
 
