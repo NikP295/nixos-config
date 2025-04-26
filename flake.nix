97a02295f676ab658fcb5be2fccc35c8a40233e7
NikP295 <nikposedi295@gmail.com>
2025-04-26 19:05:55 +0200

    Commit timestamp: sob 26 apr 2025 19:05:55 CEST

--- flake.nix
+++ flake.nix
@@ -1,42 +1,23 @@
 {
-  description = "Main flake setup";
-
-  inputs = {
-    # NixOS official package source, using the nixos-24.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    # nixos-legion = current (legion) hostname, change probably if different idk 
-    nixosConfigurations.nixos-legion = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; dotfiles = ./dotfiles; };
-      system = "x86_64-linux";
-      modules = [
-      	#import the like... Main module you want to use (the pc youre on, unless u got the cofig from me... Make one if u did!)
-        ./hosts/legion.nix
+    nixosConfigurations = {
+      nixos-thinkchad = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; dotfiles = ./dotfiles; };
+        system = "x86_64-linux";
+        modules = [
+          ./modules/home-import.nix
+          ./hosts/thinkchad.nix
+        ];
+      };
 
-
-	# i feel like touching ts (home manager) would result in instant death so double check before u do idk
-        home-manager.nixosModules.home-manager
-
-        ({ config, pkgs, ... }: {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-	  
-	  home-manager.backupFileExtension = "hm-backup";
-
-	  home-manager.users.nick = import ./home_nick.nix { inherit pkgs config; };
-
-        })
-      ];
+      nixos-legion = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; dotfiles = ./dotfiles; };
+        system = "x86_64-linux";
+        modules = [
+          ./modules/home-import.nix
+          ./hosts/legion.nix
+        ];
+      };
     };
   };
-
-
 }
-
---
+++ hosts/thinkchad.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+
+{
+
+  imports = [
+    ../hardware/legion-hw.nix
+    ../modules/common.nix
+    ../modules/plasma/plasma.nix
+    ../modules/programming.nix
+  ];
+  
+  nixpkgs.config.allowUnfree = true;
+
+  networking.hostName = "nixos-thinkchad";
+ 
+
+  boot.kernelModules = [ "amdgpu" ];
+ 
+  hardware.opengl.enable = true;
+  hardware.opengl.driSupport = true;
+  hardware.opengl.driSupport32Bit = true;
+
+  hardware.cpu.amd.updateMicrocode = true;
+
+
+
+  # Wayland support
+  services.xserver.enable = true;
+}
+
