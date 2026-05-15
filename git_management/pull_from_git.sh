1bac93f952b8d8117902f1a1bd81af86b2b41952
NikP295 <nikposedi295@gmail.com>
2026-05-16 01:10:28 +0200

    Commit timestamp: sob 16 maj 2026 01:10:28 CEST

--- git_management/pull_from_git.sh
+++ git_management/pull_from_git.sh
@@ -2,10 +2,8 @@ git -C /etc/nixos add .
 
 current_time=$(date)
 
-git -C /etc/nixos commit -m "Commit timestamp: $current_time"
+git -C /etc/nixos pull
 
-git -C /etc/nixos push -u origin main
-
-echo "Big W, associated timestamp: $current_time"
+echo "Big W, pulled from git!"
 
 
